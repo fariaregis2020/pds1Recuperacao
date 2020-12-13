78bfe17bbc2b2211aaf52277292ff177a9f55e12
fariaregis2020 <regis.pontual.2020@gmail.com>
2020-12-13 20:13:41 -0300

    Category

--- src/main/java/com/iftm/course/config/TesteConfig.java
+++ src/main/java/com/iftm/course/config/TesteConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.iftm.course.entities.Category;
 import com.iftm.course.entities.Order;
 import com.iftm.course.entities.User;
 import com.iftm.course.entities.enuns.OrderStatus;
+import com.iftm.course.repositories.CategoryRepository;
 import com.iftm.course.repositories.OrderRepository;
 import com.iftm.course.repositories.UserRepository;
 
@@ -20,10 +22,12 @@ public class TesteConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -31,21 +35,25 @@ public class TesteConfig implements CommandLineRunner {
 		User u1 = new User(1l, "nome", "email", "phone", "password");
 		User u2 = new User(2l, "nome2", "email2", "phone2", "password2");
 
-		//gambiarra 
+		// gambiarra
 		User u3 = new User();
 		u3.setNome("nome3");
 		u3.setEmail("email3");
 		u3.setPhone("phone3");
 		u3.setPassword("pass3");
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.valueOf(1));
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.valueOf(2));
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.valueOf(2));
 		
 		
-		Order o1 = new Order(null , Instant.parse("2019-06-20T19:53:07Z" ), u1, OrderStatus.valueOf(1) );
-		Order o2 = new Order(null , Instant.parse("2019-07-21T03:42:10Z" ), u2, OrderStatus.valueOf(2) );
-		Order o3 = new Order(null , Instant.parse("2019-07-22T15:21:22Z" ), u1, OrderStatus.valueOf(2) );
-		
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null,"Books");
+		Category cat3 = new Category(null, "Computers");
+				
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));	
 
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
-
