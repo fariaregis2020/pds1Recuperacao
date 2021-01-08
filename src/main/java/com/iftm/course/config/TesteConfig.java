e48f29e57d460b82a06f59312a963eaa68316aac
fariaregis2020 <regis.pontual.2020@gmail.com>
2021-01-08 20:16:51 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/iftm/course/config/TesteConfig.java
+++ src/main/java/com/iftm/course/config/TesteConfig.java
@@ -30,52 +30,90 @@ public class TesteConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
 
 	@Override
 	public void run(String... args) throws Exception {
-	
-		Category cat1 = new Category(1l,"Eletronics");
-		Category cat2 = new Category(2l,"Componet");
-		Category cat3 = new Category(3l,"Computery");
+
+		Category cat1 = new Category(1l, "Eletronics");
+		Category cat2 = new Category(2l, "Componet");
+		Category cat3 = new Category(3l, "Computery");
+
+		/*
+		 * Product p1 = new Product(null , "The Lord of the Rings" ,
+		 * "Lorem ipsum dolor sit amet, consectetur." , 90.5, ""); Product p2 = new
+		 * Product(null , "Smart TV" , "Nulla eu imperdiet purus. Maecenas ante." ,
+		 * 2190.0, ""); Product p3 = new Product(null , "Macbook Pro" ,
+		 * "Nam eleifend maximus tortor, at mollis." , 1250.0, ""); Product p4 = new
+		 * Product(null , "PC Gamer" , "Donec aliquet odio ac rhoncus cursus." , 1200.0,
+		 * ""); Product p5 = new Product(null , "Rails for Dummies" ,
+		 * "Cras fringilla convallis sem vel faucibus." , 100.99, "");
+		 */ 
 		
-	
+		
+		/*Gambiarra pois como o exemplo da aula não roda*/
+
+		Product p1 = new Product();
+		// p.setId(null);
+		p1.setName("Produto");
+		p1.setPrice(15.50);
+		p1.setDescription("Descrição do produto");
+		p1.setImgUrl("http://www.produto.com");
+
+		Product p2 = new Product();
+		// p.setId(null);
+		p2.setName("Produto");
+		p2.setPrice(15.50);
+		p2.setDescription("Descrição do produto");
+		p2.setImgUrl("http://www.produto.com");
+
+		Product p3 = new Product();
+		// p.setId(null);
+		p3.setName("Produto");
+		p3.setPrice(15.50);
+		p3.setDescription("Descrição do produto");
+		p3.setImgUrl("http://www.produto.com");
+
+		Product p4 = new Product();
+		// p.setId(null);
+		p4.setName("Produto");
+		p4.setPrice(15.50);
+		p4.setDescription("Descrição do produto");
+		p4.setImgUrl("http://www.produto.com");
+
+		Product p5 = new Product();
+		// p.setId(null);
+		p5.setName("Produto");
+		p5.setPrice(15.50);
+		p5.setDescription("Descrição do produto");
+		p5.setImgUrl("http://www.produto.com");
+		
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		User u1 = new User(1l, "Carlos", "carlos@gmail.com", "34996805000", "123d");
 		User u2 = new User(2l, "kamila", "kamila@email.com", "323425648", "214577");
 		User u3 = new User(3l, "Jeferson", "jeferson@email.com", "88081414", "8852858");
-	
 
-	   
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.valueOf(1));
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.valueOf(2));
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3, OrderStatus.valueOf(2));
-		
-		
-		Product p = new Product();
-		//p.setId(null);
-		p.setName("Produto");
-		p.setPrice(15.50);
-		p.setDescription("Descrição do produto");
-		p.setImgUrl("http://www.produto.com");
-		
-		
-		/*
-		Product p1 = new Product(null , "The Lord of the Rings" , "Lorem ipsum dolor sit amet, consectetur." , 90.5, "");
-		Product p2 = new Product(null , "Smart TV" , "Nulla eu imperdiet purus. Maecenas ante." , 2190.0, "");
-		Product p3 = new Product(null , "Macbook Pro" , "Nam eleifend maximus tortor, at mollis." , 1250.0, "");
-		Product p4 = new Product(null , "PC Gamer" , "Donec aliquet odio ac rhoncus cursus." , 1200.0, "");
-		Product p5 = new Product(null , "Rails for Dummies" , "Cras fringilla convallis sem vel faucibus." , 100.99, "");
-		*/
-	
-				
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		//productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-		productRepository.saveAll(Arrays.asList(p));
 
-
+		//categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		// productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		//productRepository.saveAll(Arrays.asList(p));
 
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/iftm/course/entities/Product.java
+++ src/main/java/com/iftm/course/entities/Product.java
@@ -8,28 +8,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
-@Table(name ="tb_product")
+@Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id ;
+	private long id;
 	private String name;
 	private String description;
-	private Double price ;
-	private String imgUrl ;
-	
-	@Transient
-	private Set<Category>categories = new HashSet<>();
-	
-	public Product () {
-		
+	private Double price;
+	private String imgUrl;
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id")
+
+	)
+
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
+
 	}
 
 	public Product(long id, String name, String description, Double price, String imgUrl) {
@@ -106,10 +111,5 @@ public class Product implements Serializable {
 			return false;
 		return true;
 	}
-
-	
-	
-	
-	
 
 }
